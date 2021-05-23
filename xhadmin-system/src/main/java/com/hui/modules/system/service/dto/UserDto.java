f32115136fbdc5d970316753e1069120ffe39783
xuzewei02 <haianxzw3@163.com>
2021-05-23 22:58:34 +0800

    个人项目第一次上传

---
+++ xhadmin-system/src/main/java/com/hui/modules/system/service/dto/UserDto.java
@@ -0,0 +1,53 @@
+package com.hui.modules.system.service.dto;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.hui.base.BaseDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * @author Admin
+ * @date 2021/5/23
+ * @time 0:48
+ */
+@Getter
+@Setter
+public class UserDto extends BaseDTO implements Serializable {
+    private Long id;
+
+    private Set<RoleSmallDto> roles;
+
+    private Set<JobSmallDto> jobs;
+
+    private DeptSmallDto dept;
+
+    private Long deptId;
+
+    private String username;
+
+    private String nickName;
+
+    private String email;
+
+    private String phone;
+
+    private String gender;
+
+    private String avatarName;
+
+    private String avatarPath;
+
+    @JSONField(serialize = false)
+    private String password;
+
+    private Boolean enabled;
+
+    @JSONField(serialize = false)
+    private Boolean isAdmin = false;
+
+    private Date pwdResetTime;
+}
