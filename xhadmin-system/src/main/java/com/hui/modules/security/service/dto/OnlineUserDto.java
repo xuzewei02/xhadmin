8ba8176d79ada898f10212ddef2658a2c1cd0441
xuzewei02 <haianxzw3@163.com>
2021-05-26 23:38:34 +0800

    解決没有序列化的问题

--- xhadmin-system/src/main/java/com/hui/modules/security/service/dto/OnlineUserDto.java
+++ xhadmin-system/src/main/java/com/hui/modules/security/service/dto/OnlineUserDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -15,7 +16,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class OnlineUserDto {
+public class OnlineUserDto implements Serializable {
     /**
      * 用户名
      */
